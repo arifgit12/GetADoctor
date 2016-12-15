0fb25bb4dcfa703cac8c7b3ff2b2179dc3393960
ARIF MONDAL <ARIF MONDAL>
2016-12-15 13:53:38 +0300

    GetADoctor - Models - Intial Models created

---
+++ GetADoctor/GetADoctor.Models/ApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetADoctor.Models
+{
+    public enum GENDER
+    {
+        MALE,
+        FEMALE
+    }
+    public enum UserRoles
+    {
+        Administrator = 1,
+        Doctor = 2,
+        Patient = 4,
+        User = 8,
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
