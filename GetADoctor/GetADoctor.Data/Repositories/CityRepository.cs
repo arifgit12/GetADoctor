928d4e3fb886e289daa729f0f91fcf51b961c613
ARIF MONDAL <ARIF MONDAL>
2016-12-16 13:51:48 +0300

    GAD - Data - City Repository and Service added

---
+++ GetADoctor/GetADoctor.Data/Repositories/CityRepository.cs
@@ -0,0 +1,22 @@
+﻿using GetADoctor.Data.Infrastructure;
+using GetADoctor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetADoctor.Data.Repositories
+{
+    public interface ICityRepository : IRepository<City>
+    { 
+
+    }
+    public class CityRepository : Repository<City>, ICityRepository
+    {
+        public CityRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ GetADoctor/GetADoctor.Data/Services/CityService.cs
@@ -0,0 +1,39 @@
+﻿using GetADoctor.Data.Repositories;
+using GetADoctor.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GetADoctor.Data.Services
+{
+    public interface ICityService
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int id);
+        int SaveCity(City city);
+    }
+    public class CityService : ICityService
+    {
+        private readonly ICityRepository cityRepository;
+
+        public CityService(ICityRepository cityRepository)
+        {
+            this.cityRepository = cityRepository;
+        }
+   
+        public IEnumerable<City> GetCities()
+        {
+            return this.cityRepository.GetAll();
+        }
+
+        public City GetCity(int id)
+        {
+            return this.cityRepository.Get(id);
+        }
+
+        public int SaveCity(City city)
+        {
+            this.cityRepository.Add(city);
+            return this.cityRepository.SaveChanges();
+        }
+    }
+}
