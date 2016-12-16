79a10b5b46d653a92e3e6e1f52aacff465c652b0
ARIF MONDAL <ARIF MONDAL>
2016-12-16 13:12:31 +0300

    GAD - Web - AutoMapper library and configuration added

--- GetADoctor/GetADoctor.Web/Global.asax.cs
+++ GetADoctor/GetADoctor.Web/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using GetADoctor.Web.Infrastructure.Mapping;
+using System;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +14,8 @@ namespace GetADoctor.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperConfiguration.RegisterMaps();
         }
     }
 }
---
+++ GetADoctor/GetADoctor.Web/Infrastructure/Mapping/AutoMapperConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetADoctor.Web.Infrastructure.Mapping
+{
+    public class AutoMapperConfiguration
+    {
+        public static void RegisterMaps()
+        {
+            AutoMapper.Mapper.Initialize(config => 
+            {
+
+            });
+        }
+    }
+}
