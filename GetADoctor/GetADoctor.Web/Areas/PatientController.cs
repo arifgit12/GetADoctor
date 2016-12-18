94be857b93aaa995de7f47b4ea7376db977cc31f
ARIF MONDAL <ARIF MONDAL>
2016-12-19 00:12:48 +0300

    GAD - Web - Patient controller and view added

---
+++ GetADoctor/GetADoctor.Web/Areas/PatientController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetADoctor.Web.Areas
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Patient/Appointments
+        public ActionResult Appointments()
+        {            
+            return View();
+        }
+
+        // GET: Patient/Appointment
+        public ActionResult Appointment()
+        {           
+            return View();
+        }
+    }
+}
