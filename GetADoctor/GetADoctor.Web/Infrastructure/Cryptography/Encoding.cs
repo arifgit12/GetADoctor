a7e0e4d1974707edd835e0f6ed0662787fbaab5b
ARIF MONDAL <arifali.mondal@gmail.com>
2017-03-09 23:49:12 +0300

    file helpers and encoding added.

---
+++ GetADoctor/GetADoctor.Web/Infrastructure/Cryptography/Encoding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetADoctor.Web.Infrastructure.Cryptography
+{
+    public class Encoding
+    {
+        public static string EncrytedString(string str)
+        {
+            string encryptedString = "";
+            byte[] encryptedValue = System.Text.Encoding.Unicode.GetBytes(str);
+            encryptedString = Convert.ToBase64String(encryptedValue);
+            return encryptedString;
+        }
+
+        public static string DecrytedString(string str)
+        {
+            string decryptedString = "";
+            byte[] decryptedValue = Convert.FromBase64String(str);
+            decryptedString = System.Text.Encoding.Unicode.GetString(decryptedValue, 0, decryptedValue.Length);
+            return decryptedString;
+        }
+    }
+}
---
+++ GetADoctor/GetADoctor.Web/Infrastructure/FileHelper/FileUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace GetADoctor.Web.Infrastructure.FileHelper
+{
+    public class FileUtils
+    {
+        public static readonly string UPLOAD_PATH = "~/Content/Uploads/Account/";
+
+        public static string UploadFile(HttpPostedFileBase file)
+        {
+            string filename = null;
+
+            if (file != null && file.ContentLength > 0)
+            {
+                filename = GetFileName(file.FileName);
+                string path = Path.Combine(HostingEnvironment.MapPath(UPLOAD_PATH), filename);
+                file.SaveAs(path);
+            }
+
+            return filename;
+        }
+
+        public static string UploadFile(HttpPostedFileBase file, string fileFolder)
+        {
+            string filename = null;
+            string filePath = UPLOAD_PATH + fileFolder;
+
+            if (file != null && file.ContentLength > 0)
+            {
+                filename = GetFileName(file.FileName);
+                bool exists = Directory.Exists(HostingEnvironment.MapPath((filePath)));
+                if (!exists)
+                {
+                    Directory.CreateDirectory(HostingEnvironment.MapPath(filePath));
+                }
+
+                string path = Path.Combine(HostingEnvironment.MapPath(filePath), filename);
+                file.SaveAs(path);
+            }
+
+            return filename;
+        }
+
+        public static string GetFileName(string uploadedFileName)
+        {
+            string filename = Guid.NewGuid() + Path.GetExtension(uploadedFileName);
+            return filename;
+        }
+    }
+}
