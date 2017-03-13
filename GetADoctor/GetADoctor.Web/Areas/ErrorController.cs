46f219b3afe1789bd57f07797538b990531f503c
ARIF MONDAL <arifali.mondal@gmail.com>
2017-03-13 23:20:59 +0300

    error controller and view added

---
+++ GetADoctor/GetADoctor.Web/Areas/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetADoctor.Web.Areas
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ViewResult Index()
+        {
+            return this.View("Error");
+        }
+
+        public ViewResult NotFound()
+        {
+            this.Response.StatusCode = 404;
+            return this.View();
+        }
+
+        public ViewResult ServerError()
+        {
+            this.Response.StatusCode = 500;
+            return this.View();
+        }
+    }
+}
