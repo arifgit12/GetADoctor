fb4d2237516b9c5734f23c0fd8a6b272ddfae0db
ARIF MONDAL <arifali.mondal@gmail.com>
2017-03-27 20:34:40 +0300

    Admin - users management controller and view added

---
+++ GetADoctor/GetADoctor.Web/Areas/Admin/Controllers/UsersManageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetADoctor.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class UsersManageController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        // GET: Admin/UsersManage
+        public ActionResult Index()
+        {
+            var users = UserManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
